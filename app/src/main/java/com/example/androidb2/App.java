fb39e51a9947961e24b08a0787dd3aafecc2a193
bar mizrahi <Barm471@gmail.com>
2023-07-14 16:26:14 +0300

    done

--- app/src/main/java/com/example/androidb2/App.java
+++ app/src/main/java/com/example/androidb2/App.java
@@ -2,7 +2,8 @@ package com.example.androidb2;
 
 import com.example.login.AppLogin;
 
-public class App extends AppLogin {
+public
+class App extends AppLogin {
     @Override
     public void onCreate() {
         super.onCreate();
---
+++ app2/src/main/java/com/example/app2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.widget.TextView;
+import com.example.textformatter.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        TextFormatter textFormatter = new TextFormatter();
+        // Format text with different options
+        String text = "Welcome to the Text Formatter Library! *_This text_* is formatted with bold and italic styles. ^Here^ is an underlined text.";
+        Spannable formattedText = textFormatter.formatText(text, false, true, false, "#FF0000");
+
+        // Display the formatted text
+        textView.setText(formattedText);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
