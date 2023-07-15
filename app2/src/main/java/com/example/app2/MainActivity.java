acc25ea2d4c17309c60607f5d586708c780e3137
bar mizrahi <Barm471@gmail.com>
2023-07-15 14:44:40 +0300

    done

--- app2/src/main/java/com/example/app2/MainActivity.java
+++ app2/src/main/java/com/example/app2/MainActivity.java
@@ -3,20 +3,17 @@ package com.example.app2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
 import android.widget.TextView;
 import com.example.textformatter.*;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         TextFormatter textFormatter = new TextFormatter();
         // Format text with different options
         String text = "Welcome to the Text Formatter Library! *_This text_* is formatted with bold and italic styles. ^Here^ is an underlined text.";
@@ -24,16 +21,5 @@ public class MainActivity extends AppCompatActivity {
 
         // Display the formatted text
         textView.setText(formattedText);
-
-
-
-
-
-
-
-
-
-
-
     }
 }
